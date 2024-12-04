b611df468b2b976afb1aec157acaeeac5a78b08e
Max Walter <walt@walt-dev.de>
2024-12-04 14:01:13 +0100

    new build files and added github action

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'plugin.js',
+        assetFileNames: 'plugin.css',
+        chunkFileNames: "chunk.js",
+        manualChunks: undefined,
+      }
+    }
+  }
 })
